f757d13b5c5ce7c4260047244c74ac32a34dca07
Justin Burris <713795+justinbburris@users.noreply.github.com>
2021-10-02 16:11:08 -0400

    Switch out the git profile to enable light/dark mode
    This isn't ideal but it works

--- osx/dotfiles/iterm2-scripts/auto_dark_mode.py
+++ osx/dotfiles/iterm2-scripts/auto_dark_mode.py
@@ -2,6 +2,7 @@
 
 import asyncio
 import iterm2
+import subprocess
 
 async def main(connection):
     async with iterm2.VariableMonitor(connection, iterm2.VariableScopes.APP, "effectiveTheme", None) as mon:
@@ -11,10 +12,26 @@ async def main(connection):
 
             # Themes have space-delimited attributes, one of which will be light or dark.
             parts = theme.split(" ")
+
+            bashCommand = '''
+                unlink /Users/jbburris/.gitconfig
+            '''
+            process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+            output, error = process.communicate()
+
             if "dark" in parts:
                 preset = await iterm2.ColorPreset.async_get(connection, "Solarized Dark")
+                bashCommand = '''
+                    ln -s /Users/jbburris/dev/personal/setup/osx/dotfiles/gitconfig.dark /Users/jbburris/.gitconfig
+                '''
             else:
                 preset = await iterm2.ColorPreset.async_get(connection, "Solarized Light")
+                bashCommand = '''
+                    ln -s /Users/jbburris/dev/personal/setup/osx/dotfiles/gitconfig.light /Users/jbburris/.gitconfig
+                '''
+
+            process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+            output, error = process.communicate()
 
             # Update the list of all profiles and iterate over them.
             profiles=await iterm2.PartialProfile.async_query(connection)

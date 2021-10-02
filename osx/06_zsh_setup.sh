01301479f047af5bf8008b395d7443cbc0d182ff
Justin Burris <713795+justinbburris@users.noreply.github.com>
2021-10-02 11:07:34 -0400

    update zsh install script to be more robust

--- osx/06_zsh_setup.sh
+++ osx/06_zsh_setup.sh
@@ -1,4 +1,11 @@
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if [ -d "~/.oh-my-zsh" ]; then
+  echo "Installing oh my zsh..."
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
 
+echo "Linking configs"
 rm ~/.zshrc
 ln -s ~/dev/personal/setup/osx/dotfiles/zshrc ~/.zshrc
+
+rm ~/.zshenv
+ln -s ~/dev/personal/setup/osx/dotfiles/zshenv ~/.zshenv

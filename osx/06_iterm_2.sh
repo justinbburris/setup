a4b742dbbdc30c646c57f4f56a604e08f852c5b9
Justin Burris <713795+justinbburris@users.noreply.github.com>
2021-10-02 11:55:48 -0400

    automatically 🪄 install scripts with iterm setup script

--- osx/06_iterm_2.sh
+++ osx/06_iterm_2.sh
@@ -1,3 +1,6 @@
 echo "Setting up iTerm2 preferences..."
 rm ~/Library/Preferences/com.googlecode.iterm2.plist
 ln -s ~/dev/personal/setup/osx/dotfiles/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
+
+mkdir -p ~/Library/Application\ Support/iTerm2/Scripts/
+ln -s ~/dev/personal/setup/osx/dotfiles/iterm2-scripts ~/Library/Application\ Support/iTerm2/Scripts/AutoLaunch

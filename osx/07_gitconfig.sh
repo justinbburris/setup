c176ceb5010472fd3bd74d5ac88c081a1c68f85b
Justin Burris <713795+justinbburris@users.noreply.github.com>
2021-10-02 15:44:12 -0400

    dark/light theme configs for git

--- osx/07_gitconfig.sh
+++ osx/07_gitconfig.sh
@@ -1,13 +1,25 @@
 rm ~/.gitconfig
 
+# dark config
 cp ~/dev/personal/setup/osx/dotfiles/gitconfig.template \
-  ~/dev/personal/setup/osx/dotfiles/gitconfig
+  ~/dev/personal/setup/osx/dotfiles/gitconfig.dark
+sed -i '' -e "s/SYNTAX_THEME/Solarized\ \(Dark\)/g" \
+  ~/dev/personal/setup/osx/dotfiles/gitconfig.dark
+
+# light config
+cp ~/dev/personal/setup/osx/dotfiles/gitconfig.template \
+  ~/dev/personal/setup/osx/dotfiles/gitconfig.light
+sed -i '' -e "s/SYNTAX_THEME/Solarized\ \(Light\)/g" \
+  ~/dev/personal/setup/osx/dotfiles/gitconfig.light
 
 gpg --list-secret-keys --keyid-format=long
 echo "Please enter GPG signing key"
 read signingkey
 
 sed -i '' -e "s/SIGNING_KEY/$signingkey/g" \
-  ~/dev/personal/setup/osx/dotfiles/gitconfig
+  ~/dev/personal/setup/osx/dotfiles/gitconfig.dark
 
-ln -s ~/dev/personal/setup/osx/dotfiles/gitconfig ~/.gitconfig
+sed -i '' -e "s/SIGNING_KEY/$signingkey/g" \
+  ~/dev/personal/setup/osx/dotfiles/gitconfig.light
+
+ln -s ~/dev/personal/setup/osx/dotfiles/gitconfig.dark ~/.gitconfig
